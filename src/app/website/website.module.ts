4782c2bf65921fdb1284aefeb521684fe2e087f4
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-03-01 10:17:10 -0700

    precarga de modulos

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -8,7 +8,6 @@ import { NavComponent } from './components/nav/nav.component';
 import { SwiperModule } from 'swiper/angular';
 
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -21,7 +20,6 @@ import { LayoutComponent } from './components/layout/layout.component';
   declarations: [
     NavComponent,
     HomeComponent,
-    CategoryComponent,
     MyCartComponent,
     LoginComponent,
     RegisterComponent,
