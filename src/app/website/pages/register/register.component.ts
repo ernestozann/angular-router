ea2937dd517005d83386aee91991066e59a85b6d
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-03-02 12:13:19 -0700

    can deactivate

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ExitGuard, OnExit } from 'src/app/guards/exit.guard';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnExit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  onExit() {
+    const rta = confirm('logica desde component, estas seguro de')
+    return rta
   }
 
 }
