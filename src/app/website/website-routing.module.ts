0752a9da7edbb11a7f998f8ca7d4e534b886a7b1
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-03-02 10:43:50 -0700

    conoce a los guardianes

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -10,6 +10,8 @@ import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
+import { AuthGuard } from '../guards/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
@@ -49,6 +51,7 @@ const routes: Routes = [
       },
       {
         path: 'profile',
+        canActivate: [AuthGuard],
         component: ProfileComponent
       },
       {
